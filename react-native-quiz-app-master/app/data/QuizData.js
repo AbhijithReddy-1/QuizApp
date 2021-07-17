359c749e189ba1fdcb1c8fc45113663f68a0531c
Karthik-Reddy2002 <abhijithreddymv172002@gmail.com>
2021-07-17 21:57:04 +0530

    Main

---
+++ react-native-quiz-app-master/app/data/QuizData.js
@@ -0,0 +1,55 @@
+export default data = [
+    {
+        question: "Who develop react.js?",
+        options: [" Nike John","Nile","Jordan Walke"," Jordan mike"],
+        correct_option: "Jordan Walke"
+    },
+    {
+        question: "Who develop react.js?",
+        options: ["Apple","Facebook","Twitter","Google"],
+        correct_option: "Facebook"
+    },
+    {
+        question: "What is used in ReactJs to increase performance ",
+        options: [" Both B and c","Original DOM","Virtual DOM"," None of the above"],
+        correct_option: "Virtual DOM"
+    },
+    {
+        question: "What is state in react?",
+        options: ["A prement storage","Internal storage of the component"," Both A and B"," None of the above"],
+        correct_option: "Internal storage of the component"
+    },
+    {
+        question: "Everything in react is ",
+        options: ["Component"," Model","Method","Package"],
+        correct_option: "Component"
+    },
+    {
+        question: "At the highest level, React components have lifecycle events that fall into ",
+        options: ["Destruction","State/Property Updates","Initialization","All of the above"],
+        correct_option: "All of the above"
+    },
+    {
+        question: "React component can return how many components",
+        options: ["one","multiple","two","three"],
+        correct_option: "multiple"
+    },
+    {
+        question: "What are props into other components ",
+        options: [" Injected","Both A and C","Method","None of the above"],
+        correct_option: "Method"
+    },
+    
+    {
+        question: "React.js in MVC is?",
+        options: ["Model","Middleware","Method","Controller"],
+        correct_option: "Controller"
+    },
+   
+    {
+        question: "Must API for every ReactJs component ?",
+        options: ["SetinitialComponent","renderComponent","render","All of the above"],
+        correct_option: "renderComponent"
+    }
+    
+]
